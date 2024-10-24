de80cef70d0080458d14835ead661e964da2a02d
Lana <al.t.loureiro0@gmail.com>
2024-10-24 09:56:14 -0300

    style buttons upgrade

---
+++ src/routes/Game/StyledGame.js
@@ -0,0 +1,362 @@
+import styled from "styled-components";
+
+export const MainGame = styled.main`
+
+.bannerimg{
+    width: 100%;
+}
+
+.game-container {
+    margin-top: 100px;
+    position: relative;
+    text-align: center;
+}
+
+.game {
+  position: relative; /* O contêiner .game controla a posição do canvas e do botão */
+}
+
+.game-canvas {
+    width: 95vw; /* Ocupa toda a largura da viewport */
+    height: auto; /* Mantém a proporção da altura */
+    max-height: 95vh; /* Limite de altura baseado na viewport */
+    display: block;
+    margin: 0 auto; /* Centralizar */
+  }
+  .top{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .change{
+    display: flex;
+  }
+
+  .btns{
+    background-color: #00C0F9;
+    border: none;
+    height: 60%;
+    width: 30%;
+    color: #52035EFF;
+    font-family: "Rajdhani", sans-serif;
+    border-radius: 10px; 
+    cursor: pointer; 
+    font-size: 1.5rem; 
+    font-weight: bold; 
+    padding: 10px 25px; 
+    margin: 5px; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5), 
+                inset 0 0 5px rgba(255, 255, 255, 0.3); 
+  }
+
+
+  .button {
+  padding: 20px 40px;
+  width: max-content;
+  margin-left: 8%;
+  border-radius: 5px;
+  align-items: center;
+  justify-content: center;
+}
+
+
+.button a{
+  color:white;
+  font-family: "Rajdhani", sans-serif;
+  font-weight:bold;
+  font-style: italic;
+  font-size:36px;
+  text-align: center;
+  text-decoration:none;
+  background-color:#DC00FE;
+  display:block;
+  padding:20px 40px;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  filter: dropshadow(color=#000, offx=0px, offy=1px);
+  -webkit-box-shadow:inset 0 1px 0 #5f106b, 0 10px 0 #5f106b;
+  -moz-box-shadow:inset 0 1px 0 #5f106b, 0 10px 0 #5f106b;
+  box-shadow:inset 0 1px 0 #5f106b, 0 10px 0#5f106b;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+  transition: 0.2s;
+  cursor: pointer; /* Indica que é clicável */
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  filter: dropshadow(color=#000, offx=0px, offy=1px);
+  -webkit-box-shadow:inset 0 1px 0#5f106b, 0 10px 0 #5f106b;
+  -moz-box-shadow:inset 0 1px 0 #5f106b, 0 10px 0 #5f106b;
+  box-shadow:inset 0 0 0 #5f106b, 0 8px 0 #5f106b;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 10px;
+  border-radius: 5px;
+
+}
+
+.button a:active{
+  top:10px;
+  background-color:#DC00FE;
+  -webkit-box-shadow:inset 0 1px 0 #5f106b, inset 0 -3px 0 #5f106b;
+  -moz-box-shadow:inset 0 1px 0 #5f106b, inset 0 -3px 0 #5f106b;
+  box-shadow:inset 0 1px 0 #5f106b, inset 0 -3px 0 #5f106b;
+  text-shadow: 0px 0px 2px #e3e0e4;
+  box-shadow: 0 0 10px #e3e0e4, 0 0 20px #e3e0e4, 0 0 40px #e3e0e4;
+}
+
+.button:after{
+  content:"";
+  height:100%;
+  width:100%;
+  padding:4px;
+  position: absolute;
+  bottom:-15px;
+  left:-4px;
+  z-index:-1;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+}
+
+.points-container {
+    padding: 5px 10px;
+    border-radius: 5px;
+    font-size: 20px;
+    color: #000;
+    display: flex;
+    align-items: center;
+    color: white;
+    font-family: "Rajdhani", sans-serif;
+    font-weight: bold;
+
+    img.pointsIcon {
+      width: 27px;
+      height: 27px;
+      margin-top: 10%;
+      margin-right: 2%;
+    }
+  }
+
+.points-upgrade-container {
+    padding: 5px 10px;
+    border-radius: 5px;
+    font-size: 20px;
+    color: #000;
+    display: flex;
+    justify-content:space-around;
+    align-items: center;
+    align-content:center;
+    color: white;
+    font-family: "Rajdhani", sans-serif;
+    font-weight: bold;
+
+    img.pointsIcon {
+      width: 27px;
+      height: 27px;
+    }
+  }
+
+.pointsIcon:hover {
+    transform: rotate(20deg); 
+    overflow: hidden;
+}
+
+.radial-menu {
+    position: relative;
+    width: 200px; 
+    height: 200px;
+    margin: auto;
+}
+
+input {
+    position: absolute;
+    display: none;
+}
+
+.btn {
+    width: 70px;
+    height: 70px;
+    border-radius: 70px;
+    position: absolute;
+    overflow: hidden;
+    cursor: pointer;
+    background: #eceff1;
+    font-size: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+
+i{
+  font-family: "Rajdhani", sans-serif;
+  font-size: medium;
+  font-weight: 600;
+}
+
+.upgrade-button {
+    position: absolute;
+    transition: transform 0.3s ease, opacity 0.3s ease; /* Animação suave */
+    opacity: 0; /* Inicialmente oculto */
+    border-radius: 50%;
+    background: linear-gradient(145deg, #07e4a2, #0967e0); 
+    border: none;
+    color: white; 
+   width: 50%;
+    cursor: pointer; 
+    font-size: 1.5rem; 
+    font-weight: bold; 
+    padding: 15px 30px; 
+    margin: 15px; 
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5), 
+                inset 0 0 5px rgba(255, 255, 255, 0.3); 
+    transition: all 0.3s ease; 
+}
+
+.upgrade-button:hover {
+    background: linear-gradient(145deg, #210252, #476ed8); 
+    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.6), 
+    inset 0 0 15px rgba(255, 255, 255, 0.4);
+    transform: translateY(-4px); 
+}
+
+.upgrade-button:active {
+    box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.5),
+    inset -5px -5px 5px rgba(255, 255, 255, 0.2);
+    transform: translateY(2px); 
+}
+
+.points-upgrade-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+   
+}
+
+.pointsIcon {
+    width: 20px; /* ajuste conforme necessário */
+    margin-left: 5px;
+}
+
+.btn.toggle {
+    z-index: 10; /* para garantir que o botão de menu esteja sempre em cima */
+    
+}
+
+.btn:not(:first-child) {
+    opacity: 0;
+}
+
+
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
+svg { 
+  max-width: 100%;  
+}
+
+
+
+.cta {
+    display: flex;
+    margin: 2%;
+    position: absolute;
+    top: 30%;
+    padding: 10px 45px;
+    text-decoration: none;
+    font-family: "Rajdhani", sans-serif;
+    font-size: 40px;
+    color: white;
+    background: #6225E6;
+    transition: 1s;
+    box-shadow: 6px 6px 0 black;
+    transform: skewX(-15deg);
+}
+
+.cta:focus {
+   outline: none; 
+}
+
+.cta:hover {
+    transition: 0.5s;
+    box-shadow: 10px 10px 0 #00C0F9;
+}
+
+.cta span:nth-child(2) {
+    transition: 0.5s;
+    margin-right: 0px;
+}
+
+.cta:hover  span:nth-child(2) {
+    transition: 0.5s;
+    margin-right: 30px;
+}
+
+
+  span:nth-child(2) {
+    width: 40px;
+    margin-left: 30px;
+    position: relative;
+    top: 12%;
+  }
+  
+
+path.one {
+    transition: 0.4s;
+    transform: translateX(-60%);
+}
+
+path.two {
+    transition: 0.5s;
+    transform: translateX(-30%);
+}
+
+
+.cta:hover path.one {
+    transform: translateX(0%);
+}
+
+.cta:hover path.two {
+    transform: translateX(0%);
+}
+
+
+
+@media (max-width: 500px) {
+
+    .game-canvas {
+    width: 100%;      /* Ajusta para 100% da largura da tela */
+    height: auto;     /* Altura automática para manter a proporção */
+  }
+
+  
+    .button a {
+      font-size: 24px; 
+      padding: 6px 21px; 
+    }
+
+    .btns{
+    height: 30%;
+    width: 10%;
+  }
+
+  .btns img{
+    width: 30px;
+  }
+
+  .top{
+    justify-content: space-between;  
+  }
+
+  
+}
+
+
+
+`
--- src/routes/Game/index.jsx
+++ src/routes/Game/index.jsx
@@ -1,8 +1,149 @@
-export default function Game(){
+import React, { useEffect, useState } from 'react';
+import { MainGame } from './styledGame';
+import BannerGame from "../../assets/bGame.png";
+import Skin from "../../assets/cabide.png";
+import Pista from "../../assets/IconPista.png";
 
-    return(
-        <>
-            <h1>GAME</h1>
-        </>
-    )
-}
+const upgrades = [
+  { name: 'Velocidade Motor', label: 'motor', priceId: 'motorPrice' },
+  { name: 'Duração bateria', label: 'battery', priceId: 'batteryPrice' },
+  { name: 'Recarga bateria', label: 'recharge', priceId: 'rechargePrice' },
+  { name: 'Eficiência bateria', label: 'efficiency', priceId: 'efficiencyPrice' },
+  { name: 'Premiação', label: 'track', priceId: 'trackPrice' },
+  { name: 'Recompensa Offline', label: 'timeOff', priceId: 'timeOffPrice' },
+  { name: 'Força Turbo', label: 'powerUpgrade', priceId: 'powerUpgradePrice' },
+  { name: 'Duração Turbo', label: 'powerTeUpgrade', priceId: 'powerTeUpgradePrice' },
+  { name: 'Recarga Turbo', label: 'powerReUpgrade', priceId: 'powerReUpgradePrice' },
+];
+
+const RadialMenu = ({ upgrades, upgrade, isOpen, toggleMenu }) => (
+  <div className="radial-menu">
+    <input type="checkbox" id="toggle" checked={isOpen} readOnly />
+    <label htmlFor="toggle" onClick={toggleMenu}>
+      <div className="btn toggle">
+        <i>upgrade</i>
+      </div>
+      {upgrades.map(({ name, label, priceId }, index) => (
+        <div
+          key={label}
+          className="upgrade-button"
+          onClick={(e) => {
+            e.stopPropagation(); // Evita o fechamento do menu
+            upgrade(label, priceId);
+          }}
+          style={{
+            transform: `rotate(${index * (360 / upgrades.length)}deg) translate(200px) rotate(-${index * (360 / upgrades.length)}deg)`,
+            opacity: isOpen ? 1 : 0,
+          }}
+        >
+          <p>{name}</p>
+          <div className="points-upgrade-container">
+            <span id={label}>10</span>
+          </div>
+        </div>
+      ))}
+    </label>
+  </div>
+);
+
+const Game = () => {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const gameStarted = localStorage.getItem('gameStarted') === 'true';
+    setIsGameStarted(gameStarted);
+
+    const script = document.createElement('script');
+    script.src = 'script.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    const canvas = document.getElementById('game-canvas-id');
+
+    const resizeCanvas = () => {
+      const { offsetWidth, offsetHeight } = canvas;
+      canvas.width = offsetWidth;
+      canvas.height = offsetHeight;
+    };
+
+    resizeCanvas();
+    window.addEventListener('resize', resizeCanvas);
+
+    return () => {
+      document.body.removeChild(script);
+      window.removeEventListener('resize', resizeCanvas);
+    };
+  }, []);
+
+  const handleStartGame = () => {
+    localStorage.setItem('gameStarted', 'true');
+    window.location.reload();
+  };
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const upgrade = (label, priceId) => {
+    console.log(`Upgrade: ${label}, Price ID: ${priceId}`);
+    // Implementar lógica de upgrade
+  };
+
+  return (
+    <>
+      <img src={BannerGame} alt="" />
+      <MainGame>
+        <div className="game-container">
+          <div className='game'>
+            {isGameStarted && (
+              <div className='top'>
+                <div className="points-container">
+                  <p>
+                    <img src="pontos.png" alt="Icone Pontos" className="pointsIcon" />
+                    <span id="points">0</span>
+                  </p>
+                </div>
+                <div className="button" onClick={() => window.power()}>
+                  <a>TURBO</a>
+                </div>
+                <div className='change'>
+                  <div className="btns" onClick={() => window.changeSkin()}>
+                    <img src={Skin} alt="" />
+                  </div>
+                  <div className="btns" onClick={() => window.changeTrack()}>
+                    <img src={Pista} alt="" />
+                  </div>
+                </div>
+              </div>
+            )}
+            <canvas className="game-canvas" id="game-canvas-id"></canvas>
+          </div>
+          <div className="controls">
+            {!isGameStarted && (
+              <div className="wrapper" onClick={handleStartGame}>
+                <a className="cta" href="#">
+                  <span>START</span>
+                  <span>
+                    <svg width="50px" height="50px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                    <g id="arrow" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <path className="one" d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z" fill="#FFFFFF"></path>
+                        <path className="two" d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z" fill="#FFFFFF"></path>
+                        <path className="three" d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z" fill="#FFFFFF"></path>
+                    </g>
+                    </svg>
+                  </span>
+                </a>
+              </div>
+            )}
+            {isGameStarted && (
+              <RadialMenu upgrades={upgrades} upgrade={upgrade} isOpen={isOpen} toggleMenu={toggleMenu} />
+            )}
+          </div>
+        </div>
+      </MainGame>
+    </>
+  );
+};
+
+export default Game;
